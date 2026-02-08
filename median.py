f540f09c2b9e847e550f04e926b55341ba6b9dab
root <chamodkalupahana@gmail.com>
2026-02-08 17:57:31 +0000

    started median

--- median.py
+++ median.py
@@ -1,12 +1,16 @@
+#%%
 import torch as th
-from alg_zoo import zoo_2nd_argmax
+from alg_zoo import zoo_2nd_argmax, zoo_argmedian
 
 hidden_size = 2
-seq_len = 2
-simple_model = zoo_2nd_argmax(hidden_size, seq_len)
+seq_len = 3
+simple_model = zoo_argmedian(hidden_size, seq_len)
 
-x = th.tensor([[0, 1]], dtype=th.float32, device=simple_model.device)  # (batch=1, seq_len=2)
+#%%
+x = th.tensor([[0, 1, 2]], dtype=th.float32, device=simple_model.device)  # (batch=1, seq_len=2)
 
 with th.no_grad():
     y = simple_model(x)          # or simple_model.forward(x)
+print(simple_model)
 print(y)
+# %%
